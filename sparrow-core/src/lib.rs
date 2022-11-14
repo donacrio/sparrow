a645e5d7cef155838811d5f078db1971d2133f09
Donatien Criaud <donatien.criaud@gmail.com>
2022-11-14 14:54:27 +0100

    repo: create sparrow bin with tcp server

--- sparrow-core/src/lib.rs
+++ sparrow-core/src/lib.rs
@@ -1,4 +1,11 @@
 mod engine;
 mod storage;
+mod tcp_server;
 
-pub use engine::Engine;
+use engine::Engine;
+use tcp_server::run_tcp_server;
+
+pub fn run(capacity: usize, addr: &str) -> Result<(), std::io::Error> {
+  let _engine = Engine::<i32, i32>::new(capacity);
+  run_tcp_server(addr)
+}
